d962c8e5189e5fac222cf54bc3f6daa2b5bf3ef5
Eva Temereva <etemereva@myseneca.ca>
2015-12-02 22:59:53 -0500

    cleaned up main.js

--- js/main.js
+++ js/main.js
@@ -7,18 +7,15 @@ var slider = document.getElementById('slider');
 text_change = function (){
     slider.style.fontSize = 32 + 'px';
     subheading.innerHTML = 'Capturing Nature';
-    subheading.style.padding = 20 + 'px';
-    
+    subheading.style.padding = 20 + 'px';    
     slider.style.transition = 'all 500ms linear';
 }
 
 //events
 window.onload = function(){
-    
-    setTimeout(text_change, 2000);
-    
+    setTimeout(text_change, 2500);
     setTimeout(function() {
-        slider.style.background = '#6A9C23';}, 2500);
+    slider.style.background = '#6A9C23';}, 2500);
         
 };
 
